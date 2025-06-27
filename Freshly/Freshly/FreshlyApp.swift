1e688edb430c55e02b61b97100ba88f6dd3d3745
Gajanan Bidwai <gajanan@vyom.ai>
2025-06-27 12:27:28 +0530

    First commit

---
+++ Freshly/Freshly/FreshlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreshlyApp.swift
+//  Freshly
+//
+//  Created by Neosoft on 27/06/25.
+//
+
+import SwiftUI
+
+@main
+struct FreshlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
